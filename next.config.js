e3ba35146dd69e639eaa90ebf0fd9427d4b51bb1
bigbad <x18825407105@outlook.com>
2026-02-28 18:32:56 +0800

    Delete next.config.js

--- next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export', // 关键配置：生成纯静态 HTML 文件
-  images: {
-    unoptimized: true, // 静态导出时通常需要关闭图片优化
-  },
-};
-
-module.exports = nextConfig;
